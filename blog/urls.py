a4e00b64653015d6c19dde60df947ed23fef1be0
Jkokol2000 <jameskokol2000@gmail.com>
2024-04-22 11:16:46 -0700

    Added Delete feature to posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='display_post'),
     path("post/<int:pk>/" , views.post_detail, name="post_detail"),
     path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('<int:pk>', views.delete_post, name='delete_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.views.decorators.http import require_http_methods
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/display_posts.html', {'posts': posts})
 
 def post_detail(request, pk):
     post=get_object_or_404(Post, pk=pk)
@@ -38,3 +39,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def delete_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('/', 'display_posts')
